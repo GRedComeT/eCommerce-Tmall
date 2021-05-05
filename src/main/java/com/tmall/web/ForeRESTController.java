34910a2fe7e92b32c66dbd66bb00c208da944288
GRedComeT <a18985113153@gmail.com>
2021-05-05 14:08:41 +0800

    购买页+结算页

--- src/main/java/com/tmall/web/ForeRESTController.java
+++ src/main/java/com/tmall/web/ForeRESTController.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpSession;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -182,6 +180,28 @@ public class ForeRESTController {
             oiid = oi.getId();
         }
         return oiid;
+    }
+
+    @GetMapping("forebuy")
+    public Object buy(String[] oiid,HttpSession session){
+        List<OrderItem> orderItems = new ArrayList<>();
+        float total = 0;
+
+        for (String strid : oiid) {
+            int id = Integer.parseInt(strid);
+            OrderItem oi= orderItemService.get(id);
+            total +=oi.getProduct().getPromotePrice()*oi.getNumber();
+            orderItems.add(oi);
+        }
+
+        productImageService.setFirstProductImagesOnOrderItems(orderItems);
+
+        session.setAttribute("ois", orderItems);
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("orderItems", orderItems);
+        map.put("total", total);
+        return Result.success(map);
     }
 
 }
