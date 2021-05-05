0c576c9a1bc6f463a0ac478380edd444d42088d0
GRedComeT <a18985113153@gmail.com>
2021-05-05 15:35:48 +0800

    其他拦截器

--- src/main/java/com/tmall/config/WebMvcConfigurer.java
+++ src/main/java/com/tmall/config/WebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.tmall.config;
 
 import com.tmall.interceptor.LoginInterceptor;
+import com.tmall.interceptor.OtherInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,17 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 class WebMvcConfigurer extends WebMvcConfigurerAdapter{
-	
 
+	@Bean
+	public OtherInterceptor getOtherIntercepter() {
+		return new OtherInterceptor();
+	}
 	@Bean
 	public LoginInterceptor getLoginIntercepter() {
 		return new LoginInterceptor();
 	}
-	
+
 	@Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(getLoginIntercepter())
-        .addPathPatterns("/**");       
-    }
+	public void addInterceptors(InterceptorRegistry registry){
+		registry.addInterceptor(getOtherIntercepter())
+				.addPathPatterns("/**");
+		registry.addInterceptor(getLoginIntercepter())
+				.addPathPatterns("/**");
+	}
 }
 
