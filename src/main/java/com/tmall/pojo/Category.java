f81f99bd25df15fde2f7c726d1baa4a3cc6d5886
GRedComeT <a18985113153@gmail.com>
2021-05-05 08:36:36 +0800

    Fix-lihao-1

--- src/main/java/com/tmall/pojo/Category.java
+++ src/main/java/com/tmall/pojo/Category.java
@@ -1,13 +1,10 @@
 package com.tmall.pojo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
 
 @Entity
 @Table(name = "category")
@@ -21,6 +18,11 @@ public class Category {
 
     String name;
 
+    @Transient
+    List<Product> products;
+    @Transient
+    List<List<Product>> productsByRow;
+
     public int getId() {
         return id;
     }
@@ -33,5 +35,23 @@ public class Category {
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+
+    public List<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+    public List<List<Product>> getProductsByRow() {
+        return productsByRow;
+    }
+    public void setProductsByRow(List<List<Product>> productsByRow) {
+        this.productsByRow = productsByRow;
+    }
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
     }
 }
